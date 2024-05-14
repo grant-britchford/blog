6d4243b52f769bcc67b30890a3fb728ab7b6dc74
Grant Britchford <145594323+grant-britchford@users.noreply.github.com>
2024-05-14 12:41:00 +0000

    code debug on all pages

--- blog/models.py
+++ blog/models.py
@@ -1,43 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
-    updated_on = models.IntegerField(auto_now=True)
-    field_1 = models.CharField()
-    
+    updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on"]
-    
+
     def __str__(self):
-       return f"{self.title} | Written by {self.author}"
-    
-    
-    
+        return f"{self.title} | written by {self.author}"
+
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_author")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
-    Created_on = models.DateTimeField(auto_now_add=True)
-    challenge = models.FloatField(default=3.0)
-        
+
     class Meta:
-        ordering = ["-created_on"]
-    
+        ordering = ["created_on"]
+
     def __str__(self):
-       return f"Comment {self.title} by {self.author}"
-   
-   
+        return f"Comment {self.body} by {self.name}"
